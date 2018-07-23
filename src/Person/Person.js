f4316953c5583379a04735c866115a1b3733c64f
Nitin <nitin.j@quantinsti.com>
2018-07-24 00:41:33 +0530

    dynamic jsx

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,8 @@
 import React from 'react'
 
 const person = () => {
-    return <p>I'm a person</p>
+    return <p>I'm a person and I'm {Math.floor(Math.random() * 20)} years old.</p>
+    // TO EXECUTE JS WITHIN JSX USE {}
 }
 
 export default person
