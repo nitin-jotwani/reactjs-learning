8c81cba4159c00b14e50bcb7ce4e0dd6a101cf56
Nitin <nitin.j@quantinsti.com>
2018-07-24 00:18:12 +0530

    jsx flow

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>Hi, Nitin</h1>
-      </div>
-    )
+    // return (
+    //   <div className="App">
+    //     <h1>Hi, Nitin</h1>
+    //   </div>
+    // )
+    /*
+      Above code is using JSX. 
+      Below code is the same output but without JSX.
+      Above code is internally compiled to code like below by React.
+      Hence writing JSX is easy.
+    */
+    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, Nitin'))
   }
 }
 
