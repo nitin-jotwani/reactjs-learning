6e0f0bf71c9ba7ed02020f8537db68536b6e293b
Nitin <nitin.j@quantinsti.com>
2018-07-24 01:03:13 +0530

    state

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  // STATE IS AVAILABLE ONLY WHEN YOU EXTEND COMPONENT
+  // IF STATE CHANGES, THEN RE-RENDER HAPPENS
+  state = {
+    persons: [
+      {name: 'manu', age:'25'},
+      {name: 'tanu', age:'27'},
+      {name: 'zanu', age:'25'}
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>Hi, Nitin</h1>
-        <Person name="manu" age="25"/>
-        <Person name="tanu" age="26"/>
-        <Person name="zanu" age="27">Child Property</Person>
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>Child Property</Person>
       </div>
     )
   }
