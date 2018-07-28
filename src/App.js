95c3efb6316fb5257c357ea9d7b632329469ed03
Nitin <nitin.j@quantinsti.com>
2018-07-28 22:22:13 +0530

    event-click

--- src/App.js
+++ src/App.js
@@ -12,11 +12,25 @@ class App extends Component {
       {name: 'zanu', age:'25'}
     ]
   }
+
+  sampleMethod = () => {
+    // DONT DO THIS. REACT WILL NOT RECOGNISE THIS, INSTEAD USE A REACT'S SPECIAL METHOD, setState()
+    // this.state.persons[0].name = 'Niku'
+    this.setState({
+      persons: [
+        {name: 'niku', age:'25'},
+        {name: 'piku', age:'27'},
+        {name: 'tiku', age:'25'}
+      ]
+    })
+    // This will override above state
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, Nitin</h1>
-        <button>Switch Name</button>
+        <button onClick={this.sampleMethod}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>Child Property</Person>
