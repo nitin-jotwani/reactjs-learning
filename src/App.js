f6f7af7442bd1118b29ef68ea8d61a173d553d7f
Nitin <nitin.j@quantinsti.com>
2018-07-29 09:26:47 +0530

    ref

--- src/App.js
+++ src/App.js
@@ -13,12 +13,12 @@ class App extends Component {
     ]
   }
 
-  sampleMethod = () => {
+  sampleMethod = (newName) => {
     // DONT DO THIS. REACT WILL NOT RECOGNISE THIS, INSTEAD USE A REACT'S SPECIAL METHOD, setState()
     // this.state.persons[0].name = 'Niku'
     this.setState({
       persons: [
-        {name: 'niku', age:'25'},
+        {name: newName, age:'25'},
         {name: 'piku', age:'27'},
         {name: 'tiku', age:'25'}
       ]
@@ -30,12 +30,19 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi, Nitin</h1>
-        <button onClick={this.sampleMethod}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <button onClick={this.sampleMethod.bind(this, 'Nitonn')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}
+          click={() => this.sampleMethod('Priyonn')}/>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>Child Property</Person>
       </div>
     )
+    // THERE ARE TWO WAYS OF SENDING ARGS TO FUNCTION:
+    // USING BIND, USING ARROW FUNCTION...BIND IS PREFERRED
+    // ALSO, WHEN USING ARROW FUNCTION YOU HAVE TO USE () IN FUNCTION CALL(even if when you dont have args), COZ ITS INSIDE THE FUNCTION
+    // WHEREAS WHEN CALLING FUNCTION DIRECTLY IN {} DONT USE (), COZ THEY WILL GET TRIGGERED AUTO, SO LET REACT HANDLE INTERNALLY
   }
 }
 
