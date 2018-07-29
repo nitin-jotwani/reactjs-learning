53f30023ca48280967d766f476f76119e2996181
Nitin <nitin.j@quantinsti.com>
2018-07-29 18:47:59 +0530

    app

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    count: 0
+  }
+
+  incrCounter = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
   render() {
     return (
       <div className="App">
         <h1>Hi, Nitin</h1>
+        Current Count: {this.state.count}<br/>
+        <button onClick={this.incrCounter}>Increase Counter</button>
       </div>
     )
   }
