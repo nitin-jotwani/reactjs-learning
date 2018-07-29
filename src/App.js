715efb2498096e4d0e10c7595539523873541bd7
Nitin <nitin.j@quantinsti.com>
2018-07-29 10:39:39 +0530

    style

--- src/App.js
+++ src/App.js
@@ -36,10 +36,16 @@ class App extends Component {
     })
   }
   render() {
+    const style = {
+      backgroundColor: 'white',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
     return (
       <div className="App">
         <h1>Hi, Nitin</h1>
-        <button onClick={this.sampleMethod.bind(this, 'Nitonn')}>Switch Name</button>
+        <button style={style} onClick={this.sampleMethod.bind(this, 'Nitonn')}>Switch Name</button>
         <Person 
           name={this.state.persons[0].name} 
           age={this.state.persons[0].age}
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import './Person.css'
 const person = (props) => {
     return (
-        <div>
+        <div className="person">
             <p onClick={props.click}>I'm a {props.name} and I'm {props.age} years old.</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
