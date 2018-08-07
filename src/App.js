33c10b508cc710d1c59079ed78e11fdc7490558e
Nitin <nitin.j@quantinsti.com>
2018-08-07 07:01:48 +0530

    connected

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import {connect} from 'react-redux';
 class App extends Component {
-  state = {
-    count: 0
-  }
-
-  incrCounter = () => {
-    this.setState({
-      count: this.state.count + 1
-    })
-  }
   render() {
     return (
       <div className="App">
         <h1>Hi, Nitin</h1>
-        Current Count: {this.state.count}<br/>
-        <button onClick={this.incrCounter}>Increase Counter</button>
+        Current Count: {this.props.ctr}<br/>
+        <button onClick={this.props.onIncrementCounter}>Increase Counter</button>
       </div>
     )
   }
 }
 
-export default App;
+//WRITE HERE WHICH SLICE OF THE STATE YOU WANT TO GET IN TO THIS CONTAINER/COMPONENT
+//AND WHICH ACTIONS YOU WANT TO DISPATCH 
+const mapStateToProps = state => {
+  return {
+    ctr: state.counter
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onIncrementCounter: () => dispatch({type: 'INCREMENT'})
+  }
+}
+//ITS A FUNCTION WHICH RETURNS A FUNCTION WHICH TAKES THEN A COMPONENT AS INPUT
+//FIRST ARG IS THE CONFIG PASSED FOR THE GIVEN CONTAINER/COMPONENT
+//SECOND ARG IS YOUR ACTIONS YOU WANT TO DISPATCH
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {createStore} from 'redux';
+import reducer from './store/reducer';
+import {Provider} from 'react-redux'; // USED TO CONNECT REDUX TO REACT
+const store = createStore(reducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
